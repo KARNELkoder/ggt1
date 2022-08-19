40fb59061ec671a87e1675087f43f4c35e9669ea
KARNELkoder <106372780+KARNELkoder@users.noreply.github.com>
2022-08-19 09:56:07 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "23.03.2004"; // Дата рождения
+var fullname = "Бондар Ілля Артемович" // ФИО
+var name = "Имя" // Имя
+
+// Паспорт
+var passport_id = "176457369" // Номер паспорта
+
+// КПП
+var kpp_id = "0173656711" // ИНН
+
+// Права
+var rights_valid_until = "11.09.2023" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "09.02.2023" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
